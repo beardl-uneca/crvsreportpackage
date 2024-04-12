9a35a25decfb1139cc09f851105713d9540be957
Beardsmore <liam.beardsmore@ons.gov.uk>
2024-04-12 11:55:24 +0100

    code fixes

--- R/create_table_3_8_&_3_9.R
+++ R/create_table_3_8_&_3_9.R
@@ -1,6 +1,8 @@
 #' Creates Tables 3.8 and 3.9
 #'
 #' @param data name of data frame being used
+#' @param date_var occurrence data variable being used
+#' @param data_year year of data
 #' @param est_data data frame of estimated data being used
 #' @param by_var the occurrence year being used e.g. dobyr or dodyr
 #' @param topic whether births or deahts is being run
@@ -13,32 +15,44 @@
 #' @import tidyr
 #' @import janitor
 #'
-#' @examples t3.8 <- create_t3.8_t3.9(bth_data, bth_est, rgn, topic = "births", tablename = "Table_3_8")
+#' @examples t3.8 <- create_t3.8_and_t3.9(data = bth_data, est_data = bth_est,
+#' date_var = dobyr, data_year = 2022, by_var = rgn,
+#' topic = "births", tablename = "Table_3_8")
+
+#' t3.9 <- create_t3.8_and_t3.9(data = dth_data, est_data = dth_est,
+#'                             date_var = dodyr, data_year = 2022, by_var = rgn,
+#'                             topic = "deaths", tablename = "Table_3_9")
 #'
-create_t3.8_t3.9 <- function(data, est_data, by_var, topic = NA, tablename = NA){
+create_t3.8_and_t3.9 <- function(data, est_data, date_var, data_year, by_var, topic = NA, tablename = NA){
   by_var <- enquo(by_var)
   by_var_name <- quo_name(by_var)
 
-counts<- data |>
-  filter(doryr == 2022 & if (topic == "births") is.na(sbind) else TRUE) |>
-  group_by(!!by_var, sex) |>
-  summarise(total = n()) |>
-  pivot_wider(names_from = sex, values_from = total, values_fill = 0) |>
-  select(-`not stated`) |>
-  adorn_totals("col")
+  counts <- data |>
+    filter({{date_var}} == data_year & if (topic == "births") is.na(sbind) else TRUE) |>
+    group_by(!!by_var, sex) |>
+    summarise(total = n()) |>
+    pivot_wider(names_from = sex, values_from = total, values_fill = 0) |>
+    select(-`not stated`) |>
+    adorn_totals("col")
 
-ests <- est_data |>
-  filter(year == 2022)
+  ests <- bth_est |>
+    filter(year == data_year) |>
+    group_by(rgn) |>
+    summarise(female_est = sum(female), male_est = sum(male)) |>
+    adorn_totals("col") |>
+    rename(total_est = Total)
 
-output <- merge(counts, ests, by = by_var_name, all.x = TRUE) |>
-  mutate(f_comp = round_excel(female.x/female.y, 2),
-         m_comp = round_excel(male.x/male.y, 2),
-         t_comp = round_excel(Total/total, 2)) |>
-  mutate(f_adj = ceiling(female.x/f_comp),
-         m_adj = ceiling(male.x/m_comp),
-         t_adj = ceiling(Total/t_comp)) |>
-  select(rgn, male.x, m_adj, female.x, f_adj, Total, t_adj)
+  output <- merge(counts, ests, by = "rgn", all.x = TRUE) |>
+    mutate(f_comp = round_excel(female/female_est, 2),
+           m_comp = round_excel(male/male_est, 2),
+           t_comp = round_excel(Total/total_est, 2)) |>
+    mutate(f_adj = ceiling(female/f_comp),
+           m_adj = ceiling(male/m_comp),
+           t_adj = ceiling(Total/t_comp)) |>
+    select(rgn, male, m_adj, female, f_adj, Total, t_adj)
 
-write.csv(output, paste0("./outputs/", tablename, ".csv"), row.names = FALSE)
-return(output)
+
+  write.csv(output, paste0("./outputs/", tablename, ".csv"), row.names = FALSE)
+  return(output)
 }
+
--- R/create_table_6_1.R
+++ R/create_table_6_1.R
@@ -9,18 +9,18 @@
 #' @import tidyr
 #' @import janitor
 #'
-#' @examples t6.1 <- create_t6.1(dth_data, dodyr, 2022)
-#' 
-create_t6.1 <- function(data, year_var, data_year){
+#' @examples t6.1 <- create_t6.1(data = dth_data, date_var = dodyr, data_year = 2022)
+#'
+create_t6.1 <- function(data, date_var, data_year){
 output <- data |>
-  filter({{year_var}} == data_year & fic10und != "" & sex %in% c("male", "female")) |>
+  filter({{date_var}} == data_year & fic10und != "" & sex %in% c("male", "female")) |>
   group_by(sex, substr(fic10und,1,3), age_grp_lead) |>
   summarise(total = n()) |>
   pivot_wider(names_from = sex, values_from = total, values_fill = 0) |>
   arrange(age_grp_lead) |>
   rename(causecd = `substr(fic10und, 1, 3)`)
 
-output <- left_join(output, cause, by = c("causecd" = "code")) 
+output <- left_join(output, cause, by = c("causecd" = "code"))
 
 outputm <- output |>
   select(description, age_grp_lead, male) |>
@@ -37,10 +37,10 @@ outputf <- output |>
   slice_max(order_by = total, n = 5, with_ties = FALSE)
 
 output <- cbind(outputm, outputf)
-  
+
 colnames(output) <- c("Male_Age_Group", "Cause_Male", "countm", "Female_Age_Group", "Cause_Female", "countf")
 output <- output |>
   select(Male_Age_Group, Cause_Male, Female_Age_Group, Cause_Female)
 
-return(output) 
+return(output)
 }

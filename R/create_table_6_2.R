78511a3f93c0be73ac37afd72f1f233ec53b39d1
beardl-uneca <liambeardsmore@btinternet.com>
2024-04-13 17:48:54 +0100

    code fixes

--- R/create_table_6_2.R
+++ R/create_table_6_2.R
@@ -10,24 +10,24 @@
 #' @import janitor
 #'
 #' @examples t6.2 <- create_t6.2(dth_data, dodyr)
-create_t6.2 <- function(data, year_var){
-  year_var <- enquo(year_var)
-  year_var_name <- quo_name(year_var)
-  
+create_t6.2 <- function(data, date_var){
+  date_var <- enquo(date_var)
+  date_var_name <- quo_name(date_var)
+
 output <- data |>
-  filter(!substr(fic10und,1,1) %in% c("", "U") & sex %in% c("male", "female") & !!year_var %in% c(2018:max(!!year_var, na.rm = TRUE))) |>
-  group_by(!!year_var, substr(fic10und,1,3)) |>
+  filter(!substr(fic10und,1,1) %in% c("", "U") & sex %in% c("male", "female") & !!date_var %in% c(2018:max(!!date_var, na.rm = TRUE))) |>
+  group_by(!!date_var, substr(fic10und,1,3)) |>
   summarise(total = n()) |>
-  arrange(!!year_var) |>
-  rename(causecd = `substr(fic10und, 1, 3)`)  
+  arrange(!!date_var) |>
+  rename(causecd = `substr(fic10und, 1, 3)`)
 
 output<- left_join(output, cause, by = c("causecd" = "code")) |>
-  group_by(!!year_var, description)|>
+  group_by(!!date_var, description)|>
   summarise(total = sum(total)) |>
-  group_by(!!year_var) |>
+  group_by(!!date_var) |>
   slice_max(order_by = total, n = 10, with_ties = FALSE) |>
   mutate(rank = rank(-total)) |>
-  pivot_wider(id_cols = rank, names_from = year_var_name, values_from = description)
+  pivot_wider(id_cols = rank, names_from = date_var_name, values_from = description)
 
 return(output)
 }
